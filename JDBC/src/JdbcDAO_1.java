6459a854b51a217b791d3123af177960e5943f61
Quazi Shams Kabir <quaziraj12@gmail.com>
2023-04-30 11:40:01 +0530

    DAO_1

---
+++ JDBC/src/JdbcDAO_1.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+public class JdbcDAO_1 {
+    public static void main(String[] args) {
+
+        StudentDAO dao = new StudentDAO();
+        Student s1 = dao.getStudent(2); // getting Student type from getStudent of dao
+        System.out.println(s1.sname);
+    }
+}
+
+class StudentDAO {
+    public Student getStudent(int rollno) { // return Student type
+        Student s = new Student();
+        s.rollno = rollno;
+        try {
+            String query = "select sname from student where rollno=" + rollno;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/shams", "root", "sqlsqlDemo@1");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            rs.next(); // coz the pointer by default will point to topmost column
+            String name = rs.getString(1);
+            s.sname = name;
+            return s;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+}
+
+class Student {
+
+    int rollno;
+    String sname;
+}
